c10d8e407968efc3168d9512f8715b92118f5757
Jose Artavia <jartavia05@gmail.com>
2025-03-03 16:02:40 -0600

    []

--- l10n_cr/cr_electronic_invoice/models/res_company.py
+++ l10n_cr/cr_electronic_invoice/models/res_company.py
@@ -190,6 +190,11 @@ class CompanyElectronic(models.Model):
                 }
                 return {'value': {'phone': ''}, 'warning': alert}
 
+    @api.onchange('signature')
+    def _onchange_signature(self):
+        if self.signature:
+            self.write({'date_expiration_sign': api_facturae.p12_expiration_date(self.signature, self.frm_pin)})
+
     # -------------------------------------------------------------------------
     # PUBLIC ACTIONS
     # -------------------------------------------------------------------------
@@ -321,8 +326,8 @@ class CompanyElectronic(models.Model):
 
     def _cron_send_email_notifications(self):
         today = datetime.now()
-        date_due = self.env.user.company_id.date_expiration_sign
-        range_day = self.env.user.company_id.range_days
+        date_due = self.env.company.date_expiration_sign
+        range_day = self.env.company.range_days
 
         range_date = date_due - timedelta(days=range_day)
         if today >= range_date:
@@ -338,7 +343,7 @@ class CompanyElectronic(models.Model):
 
             template.write(template_values)
 
-            for user in self.env.user.company_id.send_user_ids:
+            for user in self.env.company.send_user_ids:
                 if user.email:
                     template.with_context(lang=user.lang).send_mail(user.id, force_send=True, raise_exception=True)
 
